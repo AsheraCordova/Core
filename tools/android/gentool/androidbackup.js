094ad7f7b4482d02e30aca7dbf69be82bfdc94af
Ashera Cordova <ashera.cordova@gmail.com>
2022-12-03 23:15:56 +0530

    Added crossplatform cleanup of folders

--- tools/android/gentool/androidbackup.js
+++ tools/android/gentool/androidbackup.js
@@ -50,7 +50,7 @@ if (process.argv[2] == 'restore') {
 	fs.removeSync(projectBasePath + "android_backup", { recursive: true, force: true });
 
 	console.log("backing up android res and tsc files.....");
-	cpx.copy("res/**", projectBasePath + "android_backup/res");
+	cpx.copy("res/**", projectBasePath + "android_backup/res", {ignore : ["config.xml", "drawable-hdpi", "drawable-ldpi", "drawable-mdpi", "drawable-xhdpi", "drawable-xxhdpi" ]});
 	cpx.copy("tsc" + "/**", projectBasePath + "android_backup/tsc", {ignore : ["node_modules", "*.lock", "android", "app", "R", "navigation", "app.ts", "ErrorDetailFragment.ts", "widget", "ErrorFragment.ts", "FragmentFactory", "app.ts"]});
 	console.log("backing up android res and tsc files finished");
 }
